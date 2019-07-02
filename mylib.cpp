b8bea35faf385334d4a693c4fbc927d76c61408f
siewertj <malgarith@outlook.com>
2019-07-02 15:57:00 -0400

    initial commit

---
+++ mylib.cpp
@@ -0,0 +1,9 @@
+// I include with quotes here as a convention to tell me this header
+//  is 'internal' to this project.
+#include "mylib.h"
+
+int add20(int x)
+{
+	return x + 20;
+}
+
---
+++ mylib.h
@@ -0,0 +1,4 @@
+#pragma once
+
+int add20(int);
+
