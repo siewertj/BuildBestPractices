41b5d39f905b7258f0d7b6667df0c128d2cec67d
siewertj <malgarith@outlook.com>
2020-04-13 19:53:10 +0000

    can now specify a sane targetname

--- gitversion/GitVersion.cmake
+++ gitversion/GitVersion.cmake
@@ -6,11 +6,19 @@ set(GITVERSION_INCLUDE_DIRECTORIES "${GITVERSION_DIRECTORY}/include")
 
 function (GITVERSION_ADD_FILE filename)
 	set(flags "")
-	set(singleValues GIT_REPOSITORY)
+	set(singleValues GIT_REPOSITORY TARGET_NAME)
 	set(multiValues "")
 
 	include(CMakeParseArguments)
 	cmake_parse_arguments(args "${flags}" "${singleValues}" "${multiValues}" ${ARGN})
+
+	if ("${args_TARGET_NAME}" STREQUAL "")
+		set(args_TARGET_NAME "${PROJECT_NAME}_gitversion")
+	endif()
+
+	if (NOT IS_ABSOLUTE "${filename}")
+		set(filename "${CMAKE_CURRENT_BINARY_DIR}/${filename}")
+	endif()
 
 	if (Git_FOUND)
 		set(GIT_CMD "${GIT_EXECUTABLE}")
@@ -24,15 +32,10 @@ function (GITVERSION_ADD_FILE filename)
 		set(GIT_CMD "")
 	endif()
 
-	# I need to sanitize the target name
-	string(REPLACE "/" "_" target_name "${filename}")
-	string(REPLACE " " "_" target_name "${target_name}")
-	string(APPEND target_name "${target_name}" "_gitversion")
-
 	# This adds the target that is run every time. This is what actually creates the
 	# gitversion file. If GIT_CMD is an empty string, then this will take care of it
 	# and essentially return a dummy version file
-	add_custom_target(${target_name}
+	add_custom_target(${args_TARGET_NAME}
 		COMMAND ${CMAKE_COMMAND} 
 			-D GIT_CMD="${GIT_CMD}"
 			-D VERSION_FILE="${filename}"
@@ -42,6 +45,6 @@ function (GITVERSION_ADD_FILE filename)
 	# This is essentially a dummy command target so that we can link against the output
 	# file. This essentially does nothing (but depends on the dummy target)
 	add_custom_command(OUTPUT ${filename}
-		DEPENDS ${target_name}
+		DEPENDS ${args_TARGET_NAME}
 	)
 endfunction()
