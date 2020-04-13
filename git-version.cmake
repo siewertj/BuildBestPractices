eb1f774cc981727b683c911761cb64e868f6b998
siewertj <malgarith@outlook.com>
2020-04-13 14:36:51 +0000

    Trying to get a better git-version file

--- git-version.cmake
+++ git-version.cmake
@@ -1,58 +1,78 @@
-#find_program(GIT_CMD git)
-set(GIT_CMD git)
+find_package(Git)
 
-#add_custom_command()
-#add_custom_target()
-# Command to possibly get the date
-#  git log -1 --format=%cd
+if (Git_FOUND)
 
-execute_process(COMMAND ${GIT_CMD} log --pretty=format:'%h' -n 1
-	OUTPUT_VARIABLE GIT_REV
-	ERROR_QUIET)
+# Call this function on a target to add version control to said target.
+function(TARGET_ADD_GITVERSION target)
+	set(prefix args)
+	set(flags "")
+	set(singleValues WORKING_DIRECTORY)
+	set(multiValues "")
 
-if ("${GIT_REV}" STREQUAL "")
-		set(GIT_REV "N/A")
-		set(GIT_DIFF "")
-		set(GIT_TAG "N/A")
-		set(GIT_BRANCH "N/A")
-else()
-	execute_process(COMMAND ${GIT_CMD} diff --shortstat
-		OUTPUT_VARIABLE GIT_DIFF)
-	if (NOT "${GIT_DIFF}" STREQUAL "")
-		set(GIT_DIFF "+")
+	include(CMakeParseArguments)
+	cmake_parse_arguments(${prefix} "${flags}" "${singleValues}" "${multiValues}" ${ARGN})
+	set(GIT_CMD "${GIT_EXECUTABLE}")
+	if (NOT "${args_WORKING_DIRECTORY}" STREQUAL "")
+		list(APPEND GIT_CMD "-C ${args_WORKING_DIRECTORY}")
 	endif()
 
-	execute_process(COMMAND ${GIT_CMD} describe -exact-match --tags
-		OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
-	execute_process(COMMAND ${GIT_CMD} rev-parse --abbrev-ref HEAD
-		OUTPUT_VARIABLE GIT_BRANCH)
+	# Processing of git args finished, now turn into actual command
+	string(REPLACE ";" " " GIT_CMD "${GIT_CMD}")
 
-	string(STRIP "${GIT_REV}" GIT_REV)
-	string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
-	string(STRIP "${GIT_DIFF}" GIT_DIFF)
-	string(STRIP "${GIT_TAG}" GIT_TAG)
-	string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
-endif()
+	execute_process(COMMAND ${GIT_CMD} log --pretty=format:'%h' -n 1
+		OUTPUT_VARIABLE GIT_REV
+		ERROR_QUIET
+	)
 
-set(VERSION
-"const char* GIT_REV=\"${GIT_REV}${GIT_DIFF}\";
-const char* GIT_TAG=\"${GIT_TAG}\";
-const char* GIT_BRANCH=\"${GIT_BRANCH}\";")
+	if ("${GIT_REV}" STREQUAL "")
+			set(GIT_REV "N/A")
+			set(GIT_DIFF "")
+			set(GIT_TAG "N/A")
+			set(GIT_BRANCH "N/A")
+	else()
+		execute_process(COMMAND ${GIT_CMD} diff --shortstat
+			OUTPUT_VARIABLE GIT_DIFF)
+		if (NOT "${GIT_DIFF}" STREQUAL "")
+			set(GIT_DIFF "+")
+		endif()
+
+		execute_process(COMMAND ${GIT_CMD} describe -exact-match --tags
+			OUTPUT_VARIABLE GIT_TAG ERROR_QUIET)
+		execute_process(COMMAND ${GIT_CMD} rev-parse --abbrev-ref HEAD
+			OUTPUT_VARIABLE GIT_BRANCH)
+
+		string(STRIP "${GIT_REV}" GIT_REV)
+		string(SUBSTRING "${GIT_REV}" 1 7 GIT_REV)
+		string(STRIP "${GIT_DIFF}" GIT_DIFF)
+		string(STRIP "${GIT_TAG}" GIT_TAG)
+		string(STRIP "${GIT_BRANCH}" GIT_BRANCH)
+	endif()
+
+
+	set(VERSION
+	"const char* GIT_REV=\"${GIT_REV}${GIT_DIFF}\";
+	const char* GIT_TAG=\"${GIT_TAG}\";
+	const char* GIT_BRANCH=\"${GIT_BRANCH}\";")
 
 
 #if (EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp
-	#file(READ ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp VERSION_)
+		#file(READ ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp VERSION_)
 #else()
-	#set(VERSION_ "")
+		#set(VERSION_ "")
 #endif()
 #if (NOT "${VERSION}" STREQUAL "${VERSION_}")
-	#file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp "${VERSION}")
+		#file(WRITE ${CMAKE_CURRENT_SOURCE_DIR}/version.cpp "${VERSION}")
 #endif()
 
-message("${GIT_REV}")
-message("${GIT_DIFF}")
-message("${GIT_TAG}")
-message("${GIT_BRANCH}")
+	message("${GIT_REV}")
+	message("${GIT_DIFF}")
+	message("${GIT_TAG}")
+	message("${GIT_BRANCH}")
 
-message("${VERSION}")
+	message("${VERSION}")
 
+endfunction()
+
+else()
+message("git not found!")
+endif()
