05ff60f8181be3808ddc9b0711c59b7bbafd634b
siewertj <malgarith@outlook.com>
2020-04-14 14:22:34 +0000

    more usage comments

--- gitversion/GitVersion.cmake
+++ gitversion/GitVersion.cmake
@@ -1,4 +1,30 @@
-message("<-- GitVersion.cmake -->")
+# --- GitVersion ---
+#
+# The purpose of this file is to help add a build version to a project. This build
+# version will be based on the git commit of the last build and will be linked
+# into the executable/library. This is not intended to replace a proper versioning
+# scheme and instead is meant to suppliment it. The idea is that this would be
+# useful between releases when debugging or trying to figure out exactly which
+# checkout was used for a specific executable build.
+#
+# As a quick note, this WILL break any type of scheme that relies on a hash to determine
+# if 2 executables are the same. You could probably find some way to hack around it
+# but I've really intended this to be the thing that determines which executable
+# belongs with which source. The idea to basically compile the version string into
+# the exe was that it means we can't end up in some weird in-between version state.
+#
+# As this version is based on the git repo, anything not in that repo will not be
+# detected in the version. If you have an ignored file (or more likely a 3rd party
+# library that is not in your repo) then this versioning scheme will not be able
+# to tell if it's changed or not. Again, I think you could probably do some stuff
+# to make that work, but my thought here is that some other tool should be managing
+# that.
+#
+# This WILL know if the repo is dirty when you try to build. In that case, the version
+# string will be whatever is checked out, but with a '+' added to indicate that
+# the working directory is not a clean checkout.
+
+message(VERBOSE "<-- GitVersion.cmake -->")
 find_package(Git)
 
 set(GITVERSION_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}")
